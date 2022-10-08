fc33e1bd81e7d884d547326f486fee90aa620f1f
HeitorBrrt <115040903+HeitorBrrt@users.noreply.github.com>
2022-10-08 18:12:39 -0300

    Create Problem1004.c

---
+++ Problem1004.c
@@ -0,0 +1,26 @@
+/*
+Simple Product
+Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
+
+Input
+The input file contains 2 integer numbers.
+
+Output
+Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n1,n2;
+
+    printf("A: ");
+    scanf("%d", &n1);
+
+    printf("B: ");
+    scanf("%d", &n2);
+    int PROD = n1*n2;
+
+    printf("PROD = %d", PROD);
+}
