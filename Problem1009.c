ba07114b769005f3cc56ea09833c401b3491907d
HeitorBrrt <115040903+HeitorBrrt@users.noreply.github.com>
2022-10-11 17:51:30 -0300

    Update Problem1009.c

--- Problem1009.c
+++ Problem1009.c
@@ -11,3 +11,24 @@ that are the seller's salary and the total value sold by him/her.
 Output
 Print the seller's total salary, according to the given example.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char name[30];
+    double fixed_salary, sales_made;
+
+    printf("Name: ");
+    scanf("%s", name);
+
+    printf("Fixed salary: U$ ");
+    scanf("%lf", &fixed_salary);
+
+    printf("Total sales made (in money): U$ ");
+    scanf("%lf", &sales_made);
+
+    double total = fixed_salary + sales_made*0.15;
+
+    printf("Total salary: U$ %.2lf", total);
+}
