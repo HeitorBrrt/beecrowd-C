d7ea050e42e06b21f80356e5ee2625b7696a2c9a
HeitorBrrt <115040903+HeitorBrrt@users.noreply.github.com>
2022-10-19 21:08:15 -0300

    Update Problem1000.c

--- Beginner/1000-1019/Problem1000.c
+++ Beginner/1000-1019/Problem1000.c
@@ -9,3 +9,10 @@ Output
 You must print the message Hello World! and then the endline as shown below.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
